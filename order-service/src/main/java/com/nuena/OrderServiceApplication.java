11f20ff4e5c1840e5f7da7342e8e4c3019a7834d
rengb <hanying@156.com>
2019-11-08 11:48:32 +0800

    order-service添加分页

--- order-service/src/main/java/com/nuena/OrderServiceApplication.java
+++ order-service/src/main/java/com/nuena/OrderServiceApplication.java
@@ -1,17 +1,17 @@
 package com.nuena;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableEurekaClient
 @EnableFeignClients
-@ComponentScan("com.nuena.mapper")
+@MapperScan("com.nuena.mapper")
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/com/nuena/controller/OrderInfoController.java
+++ order-service/src/main/java/com/nuena/controller/OrderInfoController.java
@@ -3,14 +3,12 @@ package com.nuena.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.nuena.dto.OrderListPageDTO;
-import com.nuena.entity.Order;
 import com.nuena.facade.OrderFacade;
 import com.nuena.vo.OrderListPageVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,8 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2019-11-07
  */
 @RestController
-@RequestMapping("/order")
-public class OrderController {
+@RequestMapping("/orderInfo")
+public class OrderInfoController {
 
     @Autowired
     private OrderFacade orderFacade;
