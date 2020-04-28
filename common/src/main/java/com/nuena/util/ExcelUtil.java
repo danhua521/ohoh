02726713da5bd16bcb728ac51597c9d69177bdae
rengb <hanying@156.com>
2020-04-28 20:01:34 +0800

    数据分析更新

--- common/src/main/java/com/nuena/util/ExcelUtil.java
+++ common/src/main/java/com/nuena/util/ExcelUtil.java
@@ -7,6 +7,8 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.List;
 import java.util.Map;
@@ -21,43 +23,62 @@ public class ExcelUtil {
     /**
      * excel文件生成
      *
-     * @param path        文件路径
-     * @param filename    文件名
-     * @param sheetName   excel 页签名称
-     * @param headerNames excel 列名称
-     * @param dataMapKeys 数据map的key们
-     * @param dataMapList 数据map的集合
+     * @param isOverrideFile  当文件存在时是否覆盖
+     * @param isOverrideSheet 当表单sheet存在时，是否覆盖，不覆盖又不指定sheetName时，默认生成一个新的sheetName
+     * @param path            文件路径
+     * @param filename        文件名
+     * @param sheetName       excel 表单sheet的名称
+     * @param headerNames     excel 列名称
+     * @param dataMapKeys     数据map的key们
+     * @param dataMapList     数据map的集合
      * @return
      */
-    public static boolean createExcel(String path, String filename, String sheetName, String[] headerNames, String[] dataMapKeys, List<Map<String, String>> dataMapList) {
+    public static boolean createExcel(boolean isOverrideFile, boolean isOverrideSheet, String path, String filename, String sheetName, String[] headerNames, String[] dataMapKeys, List<Map<String, String>> dataMapList) {
         boolean flag = false;
         HSSFWorkbook wb = null;
+        HSSFSheet sheet = null;
+        File file = null;
+        FileInputStream fis = null;
         FileOutputStream fos = null;
         try {
             if (StringUtil.isBlank(path) || StringUtil.isBlank(filename) || dataMapKeys == null || dataMapKeys.length == 0 || ListUtil.isEmpty(dataMapList)) {
                 return false;
             }
-
-            fos = new FileOutputStream(path + "\\" + filename + ".xls");
-            wb = new HSSFWorkbook();
-
-            HSSFSheet sheet;
             if (StringUtil.isBlank(sheetName)) {
-                sheet = wb.createSheet("Sheet1");
+                sheetName = "Sheet1";
+            }
+            file = new File(path + "\\" + filename + ".xls");
+            if (isOverrideFile || !file.exists()) {
+                wb = new HSSFWorkbook();
             } else {
+                fis = new FileInputStream(file);
+                wb = new HSSFWorkbook(fis);
+                int index = wb.getSheetIndex(sheetName);
+                if (index > -1) {
+                    if (isOverrideSheet) {
+                        wb.removeSheetAt(index);
+                    } else {
+                        sheet = wb.createSheet();
+                    }
+                }
+            }
+            if (sheet == null) {
                 sheet = wb.createSheet(sheetName);
             }
+            fos = new FileOutputStream(file);
 
-            HSSFRow row = sheet.createRow(0);
+            HSSFRow row = null;
+            int rowNum = 0;
             if (headerNames != null && headerNames.length > 0) {
+                row = sheet.createRow(rowNum);
+                rowNum++;
                 HSSFCell headCell = null;
                 for (int i = 0; i < headerNames.length; i++) {
                     headCell = row.createCell(i);
-                    headCell.setCellValue(headerNames[i]);
+                    headCell.setCellValue(StringUtil.isBlank(headerNames[i]) ? "" : headerNames[i]);
                 }
             }
 
-            int rowNum = 1;
             HSSFCell contentCell = null;
             String contentCellValue = null;
             for (Map<String, String> dataMap : dataMapList) {
@@ -66,11 +87,7 @@ public class ExcelUtil {
                 for (String dataMapKey : dataMapKeys) {
                     contentCell = row.createCell(cellNum);
                     contentCellValue = dataMap.get(dataMapKey);
-                    if (StringUtil.isBlank(contentCellValue)) {
-                        contentCell.setCellValue("");
-                    } else {
-                        contentCell.setCellValue(contentCellValue);
-                    }
+                    contentCell.setCellValue(StringUtil.isBlank(contentCellValue) ? "" : contentCellValue);
                     cellNum++;
                 }
                 rowNum++;
@@ -84,6 +101,9 @@ public class ExcelUtil {
             try {
                 if (wb != null) {
                     wb.close();
+                }
+                if (fis != null) {
+                    fis.close();
                 }
                 if (fos != null) {
                     fos.close();
@@ -111,7 +131,7 @@ public class ExcelUtil {
         list.add(map);
         list.add(map2);
 
-        ExcelUtil.createExcel("C:\\Users\\RGB\\Desktop\\调试", "tgb", null, headerNames, dataMapKeys, list);
+        ExcelUtil.createExcel(false, false, "C:\\Users\\Administrator\\Desktop\\1234567", "med_qcresult_info", "fefefefef", headerNames, dataMapKeys, list);
     }
 
 }
